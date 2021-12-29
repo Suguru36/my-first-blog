9b030aba93be07763167cd18cde3a5cef129e506
pu <to.pu.pub@gmail.com>
2021-12-30 00:39:32 +0900

    Django latest

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
@@ -16,4 +16,4 @@ class Post(models.Model):
         self.save()
 
     def __str__(self):
-        return self.title 
+        return self.title
